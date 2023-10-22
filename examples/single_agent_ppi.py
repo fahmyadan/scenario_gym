60a298e7ee74d65eb597a72498812de27b5e9626
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-10-22 20:07:36 +0100

    fix cost and step function

--- examples/single_agent_ppi.py
+++ examples/single_agent_ppi.py
@@ -6,9 +6,10 @@ from scenario_gym.scenario import Scenario
 from scenario_gym.state import State
 from scenario_gym.sensor import Sensor, RasterizedMapSensor
 from scenario_gym.manager import ScenarioManager
+from scenario_gym.utils import ArrayLike
 from scenario_gym.xosc_interface import import_scenario
 from scenario_gym.agent import Agent
-from mppi import MPPI_Base
+from mppi import MPPI_Base, SimulateBicycleModel
 from typing import Any, Dict, List, Optional, Tuple
 from scenario_gym.metrics import Metric
 from copy import deepcopy
@@ -36,7 +37,7 @@ class MPPI_Agent(Agent, MPPI_Base):
     def __init__(self, entity: Entity, controller: VehicleController, sensor: Sensor):
         super().__init__(entity, controller, sensor)
         self.entity = entity
-        self.vehicle_model = controller
+        self.vehicle_sim_controller = controller
         self.start_pose = None 
         self.goal_pose = None 
         self.velocity = None
@@ -47,27 +48,78 @@ class MPPI_Agent(Agent, MPPI_Base):
         self.sigma_steer = flags.sigma_steer
         self.sensor = sensor
 
+        self.Q = None
+        self.w = None
+        self.U = None
+
         self.control_vector = np.zeros((2,self.time_horizon))  #2 dimensions for acc + steer
 
     def init_car_model(self):
 
-        self.vehicle_model_copy =copy.copy(self.vehicle_model) 
-        return  self.vehicle_model_copy
+        self.vehicle_model = SimulateBicycleModel(dt = gym.timestep  ,axle_length= self.entity.catalog_entry.bounding_box.length)
+
+        return  self.vehicle_model
     
-    def compute_cost(self, obs: np.ndarray, step):
+    def compute_cost(self, traj, step, goal_pose):
+
+        nearest_pred_pose = traj[:, -1]
+
+        # calculate nearest pose to final goal pose 
+        for idx, pose in enumerate(traj.T):            
+            if  np.linalg.norm(pose - goal_pose) <= np.linalg.norm(nearest_pred_pose - goal_pose) :
+                nearest_pred_pose = pose
+                nearest_goal_idx = idx
+
+        
+        # #Determine local goal using reference traj as time_idx = dt * time horizon dt = state.next_t - state.t
+
+        # ref_timesteps = self.global_path[:, 0]
+
+        # time_idx = self.time_horizon * gym.timestep
+
+        # desired_timestep = time_idx + self.dynamic_state.t
+        # time_tol = 0.25
+    
+        # for idx, t in enumerate(ref_timesteps):
+        #     if np.abs(t - desired_timestep) <= time_tol:
+        #         local_goal_idx = idx
+        #         break
+        
+        # local_goal_full = self.global_path[local_goal_idx]
+        # local_x, local_y, local_yaw = local_goal_full[1], local_goal_full[2], local_goal_full[4]
+        # local_goal = np.array([local_x, local_y, local_yaw])
+
+        # #determine nearest pose to local goal
+        # nearest_local_pose = traj[:, -1]
+
+        # for idx, pose in enumerate(traj.T):            
+        #     if  np.linalg.norm(pose - local_goal) <= np.linalg.norm(nearest_pred_pose - local_goal) :
+        #         nearest_local_pose = pose
+        #         nearest_local_idx = idx
 
 
-        return 1.0
+        c_global_distance = np.linalg.norm(nearest_pred_pose[:2] - goal_pose[:2])
+        # c_local_distance = np.linalg.norm(nearest_local_pose[:2] - local_goal[:2])
+        c_global_yaw = np.linalg.norm(nearest_pred_pose[2] - goal_pose[2])
+        # c_local_yaw = np.linalg.norm(nearest_local_pose[2] - local_goal[2])
+
+        total_cost= c_global_distance +  +  (5* c_global_yaw) 
+        
+
+
+
+        return total_cost
 
     
     def get_dynamic_obs(self, obs: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
-        ego_pose, vel = obs[0], obs[1][0]
+        ego_pose = obs[0]
 
         x,y, yaw = ego_pose[0], ego_pose[1], ego_pose[3]
-        goal_x, goal_y, goal_yaw = self.goal_pose[0], self.goal_pose[1], self.goal_pose[3]
+        goal_x, goal_y, goal_yaw = self.goal_pose[1], self.goal_pose[2], self.goal_pose[4]
 
-        start = np.array([x,y, yaw, vel])
-        goal = np.array([goal_x, goal_y, goal_yaw, 0.0])
+        start = np.array([x,y, yaw])
+        goal = np.array([goal_x, goal_y, goal_yaw])
+
 
         return start, goal 
     
@@ -83,14 +135,14 @@ class MPPI_Agent(Agent, MPPI_Base):
         U: input array, first row is acceleration, second row is delta
         Returns: Tuple[acceleration:float, steer:float]
         """
-
-        start, goal = self.get_dynamic_obs(obs)
+        gym_speed = obs[1]
+        start_pose, goal_pose = self.get_dynamic_obs(obs)
 
         S = np.zeros(self.k)
         w = np.zeros(self.k)
         Q = np.zeros((self.k, 3, self.time_horizon))
         U = np.zeros((self.k, 2, self.time_horizon))
-
+        # Generate samples for k trajectories 
         for i in range(self.k):
             # noise array
             e_acc = np.random.normal(0, self.sigma_acc, self.time_horizon)
@@ -104,24 +156,49 @@ class MPPI_Agent(Agent, MPPI_Base):
             U[i, 1, :] = steer
 
             self.init_car_model()
-
+            sim_pose = deepcopy(start_pose)
+            sim_speed = deepcopy(gym_speed)
             for t in range(self.time_horizon):
                 # state for trajectory k at time t
-                x, y, yaw  = start[0], start[1], start[2]
+                x, y, yaw  = sim_pose[0], sim_pose[1], sim_pose[2]
+                
 
                 Q[i, 0, t] = x  # x
                 Q[i, 1, t] = y  # y
                 Q[i, 2, t] = yaw  # yaw
 
-                updated_poses = self.vehicle_model_copy._step(self.dynamic_state, U[i, :, t])
-                start[0] = updated_poses[0]
-                start[1] = updated_poses[1]
-                start[2] = updated_poses[3]
-                print('check')
-            print('check')
+                updated_poses, new_speed = self.vehicle_model.move(sim_pose, sim_speed, U[i, :, t])
+                sim_pose = updated_poses
+                sim_speed = new_speed
+                assert sim_pose is not start_pose and sim_speed is not gym_speed
+
+            #Final Q for sample k at end of horizon
+            Q[i, 0, t] = sim_pose[0]  # x
+            Q[i, 1, t] = sim_pose[1]  # y
+            Q[i, 2, t] = sim_pose[2]  # yaw
+
+ 
+            #Compute score / objective for trajectory k (Q) 
+
+            S[i] = self.compute_cost(Q[i, :, :], t, goal_pose)
+
         
 
-        return np.array([1.0, 0.1])
+        #Compute weights for each trajectory k
+        for k in range(self.k):
+            w[k] = np.exp(-1 / flags.lambd * (S[k] - np.min(S)))
+        w = w / np.sum(w)
+
+        u_out = np.zeros((2, self.time_horizon))
+        u_out[0] = w.dot(U[:, 0, :])
+        u_out[1] = w.dot(U[:, 1, :])
+
+        self.Q = deepcopy(Q)
+        self.w = deepcopy(w)
+        self.U = deepcopy(U)
+
+
+        return u_out
     
     def reset(self, state: State) -> None:
         """Reset the agent state at the start of the scenario."""
@@ -129,6 +206,7 @@ class MPPI_Agent(Agent, MPPI_Base):
         self.start_pose = state.poses[self.entity]
         self.current_vel = state.velocities[self.entity]
         self.goal_pose = state.scenario.ego.trajectory.data[-1]
+        self.global_path = state.scenario.ego.trajectory.data #(22,7) array (t,x,y,z,h,p,r)
         self.last_action = None
         self.last_reward = None
         self.sensor.reset(state)
@@ -139,7 +217,10 @@ class MPPI_Agent(Agent, MPPI_Base):
         
         return None 
     
-    def _step(self, state:State) :
+    def step(self, state: State) :
+        return self._step(state)
+    
+    def _step(self, obs:np.ndarray) :
 
         """
         Select an action from the current observation.
@@ -149,15 +230,14 @@ class MPPI_Agent(Agent, MPPI_Base):
         """
 
         obs = self.dynamic_state.poses[self.entity]
-        vel = self.dynamic_state.velocities[self.entity]
+        gym_speed = np.linalg.norm(self.dynamic_state.velocities[self.entity][:2])
 
-        actions = self.compute_input((obs, vel))
+        full_actions = self.compute_input((obs, gym_speed))
 
-        new_poses = self.move_car(self.dynamic_state, actions)
+        actions = full_actions[:, 0]
 
-        
 
-        return actions
+        return self.vehicle_sim_controller.step(self.dynamic_state, actions)
     
 
 class MapSensor(RasterizedMapSensor):
@@ -274,8 +354,10 @@ class MPPI_Config(ScenarioManager):
 def run(FLAGS):
 
     terminal_conditions = ["ego_off_road", "ego_collision"]
+    global gym
     gym = ScenarioGym(
         terminal_conditions=terminal_conditions,
+        timestep=0.1,
     )
     metric = EgoSpeedMetric()
     gym.metrics.append(metric)
